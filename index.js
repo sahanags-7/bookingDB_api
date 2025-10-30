f2c39355fca4f9f86a037af5d0e8363397a1cb97
sahanags-7 <sahanags351@gmail.com>
2025-10-31 00:17:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+import express from "express";
+import { connectDB } from "./config/db.js";
+const PORT=3001;
+import {Booking} from "./models/booking.js";
+const app = express();
+app.use(express.json());
+connectDB();
+app.get("/", (req, res) => {
+res.send("Welcome to the Synergia Management System API");
+});
+
+app.get("/bookings", async (req, res) => {
+  try {
+         const bookings = await Booking.find();
+         res.json(bookings);
+     } catch (error) {
+         res.status(500).json({ error: error.message });
+     }
+ });
+
+ app.get("/bookings/:id", async (req, res) => {
+     try {
+         const booking = await Booking.findById(req.params.id);
+         if (!booking) {
+             return res.status(404).json({ message: "Booking not found" });
+         }
+         res.json(booking);
+     } catch (error) {
+         res.status(500).json({ error: error.message });
+     }
+ });
+
+
+
+
+ app.put("/bookings/:id", async (req, res) => {
+     try {
+         const booking = await Booking.findByIdAndUpdate(req .params.id , req.body, {
+             new: true,
+         });
+         if (!booking) {
+             return res.status(404).json({ message: "Booking not found" });
+         }
+         res.json({ message: "Booking updated successfully", booking });
+     } catch (error) {
+         res.status(400).json({ error: error.message });
+     }
+ });
+
+
+
+app.post("/bookings", async (req, res) => {
+     try {
+         const booking=new Booking(req.body);
+         await booking.save();
+         res.status(201).json({message:"Booking created successfully",booking});
+     } catch (error) {
+         res.status(400).json({error:error.message});
+     }
+ });
+
+        
+app.delete("/bookings/:id", async (req, res) => {
+     try {
+         const booking = await Booking.findByIdAndDelete(req.params.id);
+         if (!booking) {
+             return res.status(404).json({ message: "Booking not found" });
+         }
+         res.json({ message: "Booking deleted successfully" });
+     } catch (error) {
+         res.status(500).json({ error: error.message });
+     }
+ });      
+
+
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
---
+++ models/booking.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const bookingSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    event: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export const Booking = mongoose.model("Booking", bookingSchema);
